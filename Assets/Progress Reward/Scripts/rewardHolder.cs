e633968bb41298cfbb3743777b15de4a230a07fb
Salmanhayat98 <salmanhayat16@gmail.com>
2022-10-12 10:09:14 +0500

    remove extras

--- Assets/Progress Reward/Scripts/rewardHolder.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName = ("New/Reward holder"))]
-public class rewardHolder : ScriptableObject
-{
-    public List<reward> rewards = new List<reward>();
-}
